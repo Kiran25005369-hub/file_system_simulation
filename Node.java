e30fb94a0bd1049d8c6714858bf0bdaa9078ba60
Kiran25005369-hub <kiran.25005369@jainuniversity.ac.in>
2025-11-09 11:41:17 +0530

    Node.java updated

--- Node.java
+++ Node.java
@@ -1,24 +1,33 @@
 //Tree Node
+package filesystem; // This class is part of the 'filesystem' package
+
 import java.util.ArrayList;
 import java.util.List;
-//Node represents files and dolders
-public class Node{
-    String name; //Name of the node
-    boolean isFile; //true is file, false is folder
-    Node parent; //References parent node
-    List<Node> children; //Contains subfolders/ files
 
-    public Node(String name, boolean isFile){ //Constructor for Node class
-        this.name=name;
-        this.isFile=isFile;
-        this.parent=null;
-        this.children=new ArrayList<>(); //Initialization
+// Node represents a file or a folder inside the file system tree
+public class Node {
+
+    public String name;        // Name of the file or folder
+    public boolean isFile;     // true → file, false → folder
+    public Node parent;        // Reference to the parent node (folder)
+    public List<Node> children; // List of child nodes (only for folders) subfolders/files
+
+    // Constructor: Creates a new Node with given name and type (file/folder)
+    public Node(String name, boolean isFile){
+        this.name = name;
+        this.isFile = isFile;
+        this.parent = null; // Parent will be set later when added to a folder
+        this.children = new ArrayList<>(); // Initialize list for subfiles/subfolders
     }
-    public void addChild(Node child){ //Attaches child node to current node
-        child.parent=this;
-        this.children.add(child); //Adds the child to children list
+
+    // Adds a child node under this node (used when creating new files/folders)
+    public void addChild(Node child){
+        child.parent = this; // Assign parent reference
+        this.children.add(child); // Add to folder's children list
     }
-    public void removeChild(Node child){ //Removes a child from current node's children list
-        this.children.remove(child);
+
+    // Removes a child node from this node (used when deleting files/folders)
+    public void removeChild(Node child){
+        this.children.remove(child); // Delete reference from the children list
     }
 }
